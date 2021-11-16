273fd9e5904f93a91f11fad5167df41871e8017d
mheidenreich <76790713+mheidenreich@users.noreply.github.com>
2021-11-16 13:55:01 -0500

    Update pwm100.py

--- pwm100.py
+++ pwm100.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 """
-    Program: PWM Lookup with a 100-Value Table (pwm100.py)
-    Author:  M. Heidenreich, (c) 2020
+    Program:    PWM Lookup with a 100-Value Table (pwm100.py)
+    Platform:   Raspberry Pi
+    Author:     M. Heidenreich, (c) 2020
 
     Description:
 
